dd3e51f895f7483b8c878e6c24ad1e1391711f77
file404found1 <file404found1@yandex.ru>
2019-03-02 20:28:48 +0300

    2

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Загружаем форму..."
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Static Q As Integer = 0 'Объявление новой переменной Q
+        Q = Q + 1
+        Label1.Text = "Кнопка Количество нажата " + CStr(Q) + " раз"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Кнопка Дата нажата " + CStr(Now()), "Название окошечка даты", MessageBoxButtons.YesNo)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End 'Завершение работы приложения...
+    End Sub
+End Class
