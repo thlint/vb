4093b848a60e75eadb8ac68b7978302cb6adc464
File404Found1 <File404Found1@yandex.ru>
2019-03-05 16:59:17 +0300

    Loops

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,6 +1,34 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Label1.Text = "Загружаем форму..."
+        Dim D1, D2 As DateTime
+        Dim DW, DW2 As String
+        D1 = DateTime.Now
+        D2 = D1.AddDays(100) 'Присвоение переменной D2 значения D1 + 100 дней
+        Select Case D1.DayOfWeek
+            Case DayOfWeek.Sunday
+                DW = "Воскресенье"
+            Case DayOfWeek.Monday
+                DW = "Понедельние"
+            Case DayOfWeek.Tuesday
+                DW = "Вторник"
+            Case DayOfWeek.Wednesday
+                DW = "Среда"
+            Case DayOfWeek.Thursday
+                DW = "Четверг"
+            Case DayOfWeek.Friday
+                DW = "Пятница"
+            Case DayOfWeek.Saturday
+                DW = "Суббота"
+        End Select
+        DW2 = "Сейчас: " + CStr(D1) + vbCrLf + "Сегодня: " + DW + " " + CStr(DateTime.Today) + vbCrLf +
+        "Текущее время: " + CStr(D1.Hour) + ":" + CStr(D1.Minute) + ":" + CStr(D1.Second) + vbCrLf +
+        "В этом месяце " + CStr(DateTime.DaysInMonth(D1.Year, D1.Month)) + "дня(ей)" + vbCrLf +
+        "через 100 дней будет " + CStr(D2.Date)
+        Dim DW3 As String, Num As Integer
+        Num = 74
+        DW3 = CType(Num, String) 'Преобразуем число в строку
+        MessageBox.Show(DW2 + vbCrLf + DW3)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
--- WindowsApp1/Form2.vb
+++ WindowsApp1/Form2.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form2
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim B(100) As Integer, K As Byte
+        For K = 0 To 100
+            B(K) = K * 10
+        Next
+        MessageBox.Show(CType(B(10), String))
+        Dim M() As String = {"Яблоко", "Груша", "Вишня"}
+        ' For dim i As Integer = 
+    End Sub
 End Class
