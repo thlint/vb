7acb356e0da73817b6e8a5619f253691a02c33b0
File404Found1 <File404Found1@yandex.ru>
2019-03-12 12:03:49 +0300

    db 1

---
+++ WindowsApp1/Form4.vb
@@ -0,0 +1,3 @@
+﻿Public Class MyDB
+
+End Class
--- WindowsApp1/My Project/Settings.Designer.vb
+++ WindowsApp1/My Project/Settings.Designer.vb
@@ -65,6 +65,16 @@ Namespace My
                 Me("Березкин") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\ttt.accdb")>  _
+        Public ReadOnly Property tttConnectionString() As String
+            Get
+                Return CType(Me("tttConnectionString"),String)
+            End Get
+        End Property
     End Class
 End Namespace
 
